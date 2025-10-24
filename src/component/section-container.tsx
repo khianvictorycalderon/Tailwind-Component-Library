1e87270ff906674cedc4b1567279f5610728a405
khian calderon <ckhianvictory@gmail.com>
2025-10-24 14:41:07 +0800

    Fixed incorrect function name

--- src/component/section-container.tsx
+++ src/component/section-container.tsx
@@ -4,7 +4,7 @@ interface SectionContainerProps extends React.HTMLAttributes<HTMLDivElement> {
     debugMode?: boolean;
 }
 
-export default function Container({ children, className, debugMode = false, ...props }: SectionContainerProps) {
+export default function SectionContainer({ children, className, debugMode = false, ...props }: SectionContainerProps) {
     return (
         <section className={`px-4 md:px-8 lg:px-32 overflow-hidden ${className} ${debugMode && "border-2 border-red-500" }`} {...props}>
             <div className={`max-w-7xl mx-auto ${debugMode && "border-2 border-blue-500"}`}>
